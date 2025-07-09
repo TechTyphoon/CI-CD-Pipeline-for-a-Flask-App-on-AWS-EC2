6c13c37e38bc691dde3555de858c4dbf2ada67f0
TechTyphoon <techtyphoon.html@gmail.com>
2025-07-10 01:01:45 +0530

    Trigger CI/CD

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World! CI/CD is working 🎉'
+    return 'Hello, Mahendra! CI/CD is working 🎉'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
