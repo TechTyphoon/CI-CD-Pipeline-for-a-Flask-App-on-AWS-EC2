954639e6c0799c8f4b28879fd3f1d5dcda35e119
TechTyphoon <techtyphoon.html@gmail.com>
2025-07-10 17:48:29 +0530

    chore: trigger CI/CD workflow with a minor comment

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return 'Hello, Mahendra! CI/CD is working 🎉'
 
+
+# Health check route
+@app.route('/health')
+def health():
+    return 'OK', 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
